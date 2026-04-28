dbd13d8909740f5a1128793639cc131c96d1d131
aqdas-ahmad <talhaandaqdas@gmail.com>
2026-04-28 20:18:16 +0500

    Test express+mongoose imports

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,10 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
 export default (req, res) => {
-  res.json({ status: 'pong', mongo_set: !!process.env.MONGO_URI });
+  res.json({
+    express_ok: !!express,
+    mongoose_ok: !!mongoose,
+    mongo_uri_set: !!process.env.MONGO_URI,
+  });
 };
